60ea4cfae0d8c6b92561d0957ded62db28dfbc24
aviram8919 <49782497+aviram8919@users.noreply.github.com>
2020-06-12 20:07:09 +0530

    Update ConnectionToDatabase.php

--- ConnectionToDatabase.php
+++ ConnectionToDatabase.php
@@ -1,7 +1,7 @@
 <?php
 $db_name="userinfo";  //the name of database you have created
 $mysql_username="root";//The user name of server (see from privilages)
-$mysql_password="1234";//Password of server
+$mysql_password="";//Password of server
 $server_name="localhost";//server name
 $conn=mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name); //connection method
 if($conn)
